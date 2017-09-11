0dad4958edd5e42c0b21a758adbb88eb324d0639
aroodoo_asus_hary <aroodoo@asus.aro>
2017-09-11 18:10:10 +0300

    Call all view file

--- __openerp__.py
+++ __openerp__.py
@@ -8,10 +8,14 @@
     "complexity": "normal",
     "data": [
         # "data/templates.xml", # un comment to enable js, css code
+        "data/insurance_branch_data.xml",
+        "data/insurance_fraction_data.xml",
         # "security/security.xml",
         # "security/ir.model.access.csv",
         "views/product_template_view.xml",
         "views/insurance_branch_view.xml",
+        "views/insurance_fraction_view.xml",
+        "views/account_analytic_account_view.xml",
         # "actions/act_window.xml",
         "views/menu.xml",
         # "data/data.xml",
