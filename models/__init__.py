103bb0cbd9a175ed73b06e0847b69d828b389715
aroodoo_asus_hary <aroodoo@asus.aro>
2017-09-12 18:13:43 +0300

    Load py file

--- models/__init__.py
+++ models/__init__.py
@@ -4,4 +4,7 @@ import product_template
 import insurance_branch
 import insurance_fraction
 import insurance_product
+import insurance_type_risk
 import account_analytic_account
+import analytic_history_stage
+import analytic_history
