70e3ecb5ffd4daade9ac5260bcf9bb06063cbfed
aroodoo_asus_hary <aroodoo@asus.aro>
2017-09-12 18:09:06 +0300

    add type risk model

---
+++ models/insurance_type_risk.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, exceptions, fields, models, _
+
+
+class InsuranceTypeRisk(models.Model):
+    _name = 'insurance.type.risk'
+    _description = 'Insurance type of risk'
+
+    name = fields.Char(string='Name', required=True)
+    code = fields.Char(string='Code', required=True)
+    ins_product_id = fields.Many2one(
+        string='Insurance Product', required=True,
+        comodel_name='insurance.product')
