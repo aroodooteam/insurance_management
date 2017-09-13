8e4cfa7753aacceef7ec8da2aea8a21c9a411247
aroodoo_asus_hary <aroodoo@asus.aro>
2017-09-13 16:56:20 +0300

    import new models

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,8 @@ import insurance_branch
 import insurance_fraction
 import insurance_product
 import insurance_type_risk
+import insurance_type_risk_description
+import insurance_type_risk_clause
 import account_analytic_account
 import analytic_history_stage
 import analytic_history
