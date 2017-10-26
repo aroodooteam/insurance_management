76406d872372f9b28c59324d0b7773799dd39d2e
aroodoo_asus_hary <aroodoo@asus.aro>
2017-10-26 17:27:16 +0300

    Add new function for amendment

--- wizard/analytic_history_wiz.py
+++ wizard/analytic_history_wiz.py
@@ -35,8 +35,9 @@ class AnalyticHistoryWiz(models.TransientModel):
                 raise exceptions.Warning(_('You can\'t have twice <AFN> in same contract \n [Note]'))
             elif self.stage_id.code == 'RES' and history_ids.stage_id.code not in ('RES', 'DEV'):
                 logger.info('Resilliate contract: Create history with state=RES and close current contract')
-            elif self.stage_id.code in ('AVN') and history_ids.stage_id.code not in ('RES'):
+            elif self.stage_id.code in ('AVT') and history_ids.stage_id.code not in ('RES'):
                 logger.info('Amendment contract')
+                return self.update_contract()
             elif self.stage_id.code in ('REN') and history_ids.stage_id.code not in ('RES'):
                 logger.info('Renew contract')
                 return self.renew_analytic_account()
@@ -50,7 +51,7 @@ class AnalyticHistoryWiz(models.TransientModel):
                 logger.info('New contract')
             if self.stage_id.code == 'DEV':
                 logger.info('Devis contract')
-
+        logger.info('Before returning standard view')
         return {
             'name': 'History',
             'type': 'ir.actions.act_window',
@@ -81,7 +82,7 @@ class AnalyticHistoryWiz(models.TransientModel):
             copy_vals = history_ids.with_context(ctx)._get_all_value()
             ctx.update(parent_amendment_line=history_ids.id)
             copy_vals.update(default_is_last_situation=True)
-            copy_vals.update(default_emission_id=self.env.ref('insurance_management.renouvellement').id)
+            copy_vals.update(default_stage_id=self.env.ref('insurance_management.renouvellement').id)
             copy_vals.update(default_parent_id=history_ids.id)
             ctx.update(copy_vals)
             view_id = self.env.ref('insurance_management.view_analytic_history_form').id
@@ -97,4 +98,21 @@ class AnalyticHistoryWiz(models.TransientModel):
                 'target': 'current',
                 'flags': {'form': {'action_buttons': True, 'options': {'mode': 'edit'}}},
             })
+        return res
+
+    @api.multi
+    def update_contract(self):
+        """ Update contract: Avenant """
+        history_obj = self.env['analytic.history']
+        res = self.renew_analytic_account()
+        ctx = res.get('context', {})
+        res.update(name=_('Amendment'))
+        ctx.update(version_type='amendment')
+        logger.info('\n === parent_id = %s' % ctx.get('default_parent_id', False))
+        history_id = ctx.get('default_parent_id', False)
+        history_id = history_obj.browse(history_id)
+        ctx.update(default_starting_date=history_id.starting_date)
+        ctx.update(default_ending_date=history_id.ending_date)
+        ctx.update(default_stage_id=self.env.ref('insurance_management.avenant').id)
+        res.update(context=ctx)
         return res
