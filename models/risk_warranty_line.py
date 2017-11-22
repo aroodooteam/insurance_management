73ae7bf8a17ac3de0706fceb9765ca652ec71c5d
aroodoo_asus_hary <aroodoo@asus.aro>
2017-11-22 17:11:47 +0300

    add new field to use for amount of invoice_line

--- models/risk_warranty_line.py
+++ models/risk_warranty_line.py
@@ -17,8 +17,8 @@ class RiskWarrantyLine(models.Model):
     warranty_id = fields.Many2one(
         comodel_name='product.product', string='Warranty',
         domain="[('type_risk_id', '=', type_risk_id),('is_warranty', '=', True)]")
-    yearly_net_amount = fields.Float(string='Yearly Net Amount', digits_compute=dp.get_precision('Account'), help='Yearly net amount')
-    proratee_net_amount = fields.Float(string='Yearly Net Amount', digits_compute=dp.get_precision('Account'), help='Proratee net amount')
+    yearly_net_amount = fields.Float(string='Yearly Net', digits_compute=dp.get_precision('Account'), help='Yearly net amount')
+    proratee_net_amount = fields.Float(string='Proratee Net', digits_compute=dp.get_precision('Account'), help='Proratee net amount')
 
     @api.onchange('warranty_id')
     def onchange_warranty(self):
