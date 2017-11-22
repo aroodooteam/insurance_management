95eb7dd92d8f2f11187089e4166fdb9d3517a5e8
aroodoo_asus_hary <aroodoo@asus.aro>
2017-11-22 17:57:12 +0300

    Add TODO task

--- models/risk_warranty_line.py
+++ models/risk_warranty_line.py
@@ -20,6 +20,9 @@ class RiskWarrantyLine(models.Model):
     yearly_net_amount = fields.Float(string='Yearly Net', digits_compute=dp.get_precision('Account'), help='Yearly net amount')
     proratee_net_amount = fields.Float(string='Proratee Net', digits_compute=dp.get_precision('Account'), help='Proratee net amount')
 
+    # TODO
+    # 1- Get yearly_net_amount value in onchange_warranty
+    # 2- Get proratee_net_amount value in onchange_warranty
     @api.onchange('warranty_id')
     def onchange_warranty(self):
         if not self.warranty_id:
