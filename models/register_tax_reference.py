269c2f1262a924350beee48b0a9d6864248a580f
aroodoo_asus_hary <aroodoo@asus.aro>
2017-12-15 11:19:30 +0300

    Add new function to load tax in reg_tax_reference

--- models/register_tax_reference.py
+++ models/register_tax_reference.py
@@ -2,6 +2,8 @@
 
 from openerp import api, exceptions, fields, models, _
 from openerp.addons.decimal_precision import decimal_precision as dp
+import logging
+logger = logging.getLogger(__name__)
 
 
 class RegisterTaxReference(models.Model):
@@ -16,3 +18,19 @@ class RegisterTaxReference(models.Model):
     tax_ids = fields.Many2many(comodel_name='account.tax', string='Register Tax')
     date_start = fields.Date(string='Start')
     date_end = fields.Date(string='End')
+
+
+    @api.model
+    def update_tax_ids(self):
+        acc_tx = self.env['account.tax']
+        for reg_tx in self.search([]):
+            if reg_tx.indice != 14.5:
+                domain_tx = [('amount', '=', reg_tx.indice/100), ('description', 'ilike', 'Te-%')]
+                acc_tx_ids = acc_tx.search(domain_tx)
+                vals = {'tax_ids': [(6,0,tuple(acc_tx_ids.ids))]}
+                reg_tx.write(vals)
+            elif reg_tx.indice == 14.5:
+                domain_tx = ['&',('amount', 'in', (0.045, 0.1)),'|',('description', 'ilike', 'Te-%'),('description', 'ilike', 'TACAVA-%')]
+                acc_tx_ids = acc_tx.search(domain_tx)
+                vals = {'tax_ids': [(6,0,tuple(acc_tx_ids.ids))]}
+                reg_tx.write(vals)
