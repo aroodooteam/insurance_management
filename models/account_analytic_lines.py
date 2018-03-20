ffcd7272bf3a418f9bf3b0b6fed3ead40b9740f9
aroodoo_asus_hary <aroodoo@asus.aro>
2018-03-20 14:54:29 +0300

    Add new field in analytic line

---
+++ models/account_analytic_lines.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, exceptions, fields, models, _
+from openerp.addons.decimal_precision import decimal_precision as dp
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    history_id = fields.Many2one(comodel_name='analytic.history', string='History')
+    amount_to_invoice = fields.Float(string='Amount to invoice', digits_compute=dp.get_precision('Account'))
