9448aa393640e8c7070b9859e0b73fb22f4c9f82
aroodoo_asus_hary <aroodoo@asus.aro>
2018-03-26 18:40:15 +0300

    Add new field for analytic_line

--- models/account_analytic_lines.py
+++ models/account_analytic_lines.py
@@ -8,4 +8,5 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     history_id = fields.Many2one(comodel_name='analytic.history', string='History')
+    hist_id = fields.Many2one(comodel_name='account.analytic.account', string='History')
     amount_to_invoice = fields.Float(string='Amount to invoice', digits_compute=dp.get_precision('Account'))
