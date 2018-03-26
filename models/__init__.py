4a0b408eb7e223ecf2f8fe6a5d69e711f7ca3cf7
aroodoo_asus_hary <aroodoo@asus.aro>
2018-03-26 11:53:36 +0300

    re-order import

--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,9 @@ import insurance_product
 import insurance_type_risk
 import insurance_type_risk_description
 import insurance_type_risk_clause
-import account_analytic_account
 import analytic_history_stage
 import analytic_history
+import account_analytic_account
 import analytic_history_risk_line
 import risk_warranty_line
 import risk_description_line
